97e11f66cc51b6c976dd1043c8465f3f6c10a0a5
Conqueror <ConquerorWolverine@users.noreply.github.com>
2019-12-18 11:29:43 +0800

    Final

--- Experiment7/K-means.py
+++ Experiment7/K-means.py
@@ -7,7 +7,12 @@ Created on Tue Jul  3 08:09:15 2018
 
 from sklearn.cluster import KMeans 
 import matplotlib.pyplot as plt
-import numpy as np
+import sklearn.datasets as datasets
+# import numpy as np
+
+wine = datasets.load_wine()
+wine_data = wine.data
+
 X = [[4, 10], 
      [7, 10], 
      [4, 8], 
@@ -21,17 +26,23 @@ X = [[4, 10],
      [9, 3], 
      [12, 3]]
 # dataset = np.mat(X)
-dataset = X
+dataset = wine_data
+# print(dataset)
 if __name__ == '__main__':
-    estimator = KMeans(n_clusters=4)
+    estimator = KMeans(n_clusters=13)
     # fit_predict表示拟合+预测，也可以分开写
-    res = estimator.fit_predict(dataset)
+    res = estimator.fit(dataset)
+
+    # print(res)
     # 预测类别标签结果
     lable_pred = estimator.labels_
+    print(lable_pred)
     # 各个类别的聚类中心值
     centroids = estimator.cluster_centers_
+    print(centroids)
     # 聚类中心均值向量的总和
     inertia = estimator.inertia_
+    print(inertia)
     for i in range(len(dataset)):
         if int(lable_pred[i]) == 0:
             plt.scatter(dataset[i][0], dataset[i][1], color='red')
@@ -42,4 +53,22 @@ if __name__ == '__main__':
             plt.scatter(dataset[i][0], dataset[i][1], color='blue')
         if int(lable_pred[i]) == 3:
             plt.scatter(dataset[i][0], dataset[i][1], color='green')
+        if int(lable_pred[i]) == 4:
+            plt.scatter(dataset[i][0], dataset[i][1], color='aliceblue')
+        if int(lable_pred[i]) == 5:
+            plt.scatter(dataset[i][0], dataset[i][1], color='yellow')
+        if int(lable_pred[i]) == 6:
+            plt.scatter(dataset[i][0], dataset[i][1], color='chocolate')
+        if int(lable_pred[i]) == 7:
+            plt.scatter(dataset[i][0], dataset[i][1], color='darkgray')
+        if int(lable_pred[i]) == 8:
+            plt.scatter(dataset[i][0], dataset[i][1], color='azure')
+        if int(lable_pred[i]) == 9:
+            plt.scatter(dataset[i][0], dataset[i][1], color='hotpink')
+        if int(lable_pred[i]) == 10:
+            plt.scatter(dataset[i][0], dataset[i][1], color='lightsalmon')
+        if int(lable_pred[i]) == 11:
+            plt.scatter(dataset[i][0], dataset[i][1], color='mintcream')
+        if int(lable_pred[i]) == 12:
+            plt.scatter(dataset[i][0], dataset[i][1], color='orchid')
     plt.show()
--- Experiment7/层次.py
+++ Experiment7/层次.py
@@ -11,20 +11,20 @@ import matplotlib.pyplot as plt
 iris = datasets.load_iris()  # iris 是一个有标签的数据集 
 
 iris_data = iris.data  # 直接可以得到除去标签和其他内容而得到纯数据
+
 # 生成点与点之间的距离矩阵,这里用的欧氏距离:
-# disMat = hcluster.distance.pdist(iris_data,'euclidean')
+disMat = hcluster.distance.pdist(iris_data, 'euclidean')
 # print(disMat)
 # 进行层次聚类
-# Z=hcluster.linkage(disMat,method='complete')
-# Z=iris_data
-# print (Z)
-# P = hcluster.dendrogram(Z)
-# plt.show()
+Z = hcluster.linkage(disMat, method='average')
+# print(Z)
+P = hcluster.dendrogram(Z)
+plt.show()
 
 # 根据linkage matrix Z得到聚类结果:
-result = hcluster.fclusterdata(iris_data, criterion="maxclust", t=6)
-print(iris_data)
-print(result) 
+result = hcluster.fclusterdata(Z, criterion="maxclust", t=6)
+# print(iris_data)
+# print(result)
 for i in range(len(result)):
     if int(result[i]) == 4:
         plt.scatter(iris_data[i][0], iris_data[i][1], color='red')
